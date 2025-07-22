2572ac39c8c653ab03c4506a870f8adf86dfb436
pappu9654 <22101165@uap-bd.edu>
2025-07-22 12:10:05 +0600

    Add files via upload

---
+++ SJF.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+int pi,at ,bt;
+pi={p1,p2,p3,p4};
+at={0,1,2,3};
+bt={3,2,1,4};
+#completion time
+for(int i=0;i<3;i++){
+    int ct=+bt;
+}
+printf("completion time =%d",&ct)
+#turn around time
+for(i=0;i<3;i++){
+    int tat =ct-at;
+}
+printf("turn around time =%d",&ct)
+#wating  time
+for(i=0;i<3;i++){
+    int wt =tat-bt;
+}
+printf("wating time =%d",&wt)
+
+}
